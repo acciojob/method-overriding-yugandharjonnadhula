429d9620c503d38a9139df7d4a924c0c35b55f12
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-04-02 16:12:58 +0530

    Revert "Github Repo"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,25 +1,5 @@
 package com.driver;
 
-
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-class B extends A {
-    
-    
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
-
 public class Main {
-    public static void main(String[] args) {
-        
-        B objB = new B();
-
-        
-        System.out.println(objB.meth());  
-    }
+  
 }
